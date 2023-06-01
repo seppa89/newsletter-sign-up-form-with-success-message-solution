963e7b50b60c23a1104afb6fb22d2767ed00f678
seppa89 <darek0511@gmail.com>
2023-06-01 10:23:05 +0200

    update: dynamical email in success.html, mobile viewport

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const formEl = document.querySelector("form");
 const formControlEl = document.querySelector(".form-control");
-
 const emailInput = document.querySelector('input[type="email"]');
+
+const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 function resetError() {
   formControlEl.classList.remove("invalid");
   emailInput.dataset.valid = "";
 }
-
-const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 emailInput.addEventListener("input", resetError);
 
@@ -20,7 +19,7 @@ formEl.addEventListener("submit", e => {
   const email = data.get("email");
 
   if (email.trim() && emailRegExp.test(email)) {
-    window.location.href = "/newsletter-sign-up-form-with-success-message-solution/success.html";
+    window.location.href = `/newsletter-sign-up-form-with-success-message-solution/success.html?email=${email}`;
   } else {
     e.target.reset();
     formControlEl.classList.add("invalid");
